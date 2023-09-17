a4f503beade256b5b5f9ccd1fe7bce959d20e960
nuuX <cv.bouhlaoui@gmail.com>
2023-09-17 18:40:51 +0100

    Update c7.sh

--- c7.sh
+++ c7.sh
@@ -12,7 +12,7 @@ echo ""
 echo "#--> Creating a new User..."
 user_output=$(echo 'create User email="gui@hbtn.io" password="guipwd" first_name="Guillaume" last_name="Snow"' | sudo -E HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db .//console.py)
 user_id=$(echo "$user_output" | grep -o -E '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')
-echo "User ID: $user_id"
+echo "#--> User ID: $user_id"
 
 echo ""
 # List all Users
