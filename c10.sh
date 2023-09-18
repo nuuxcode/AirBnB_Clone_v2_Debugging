355b1a83d38de2199cb6220bc002de9003285b50
nuuX <cv.bouhlaoui@gmail.com>
2023-09-18 17:22:34 +0100

    Update c10.sh

--- c10.sh
+++ c10.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+echo ""
+echo "--------------"
+echo "--- TASK10 ---"
+echo "--------------"
+echo ""
 # Drop the hbnb_dev_db database if it exists
-echo "Dropping hbnb_dev_db database if it exists..."
+echo "#--> Dropping hbnb_dev_db database if it exists..."
 echo "DROP DATABASE IF EXISTS hbnb_dev_db;" | sudo mysql
 
+echo ""
 # Execute MySQL setup script
-echo "Executing MySQL setup script..."
+echo "#--> Executing MySQL setup script..."
 cat setup_mysql_dev.sql | sudo mysql
 
+echo ""
 # Set environment variables and run the Python script
-echo "Running main_place_amenities.py..."
+echo "#--> Running main_place_amenities.py..."
 sudo -E HBNB_MYSQL_USER=hbnb_dev \
     HBNB_MYSQL_PWD=hbnb_dev_pwd \
     HBNB_MYSQL_HOST=localhost \
@@ -17,12 +24,17 @@ sudo -E HBNB_MYSQL_USER=hbnb_dev \
     HBNB_TYPE_STORAGE=db \
     ./main_place_amenities.py
 
+echo ""
 # Query amenities and places tables
-echo "Querying amenities table..."
+echo "#--> Querying amenities table..."
 echo 'SELECT * FROM amenities\G' | sudo mysql -uhbnb_dev -phbnb_dev_pwd hbnb_dev_db
 
-echo "Querying places table..."
+echo ""
+echo "#--> Querying places table..."
 echo 'SELECT * FROM places\G' | sudo mysql -uhbnb_dev -phbnb_dev_pwd hbnb_dev_db
 
-echo "Querying place_amenity table..."
+echo ""
+echo "#--> Querying place_amenity table..."
 echo 'SELECT * FROM place_amenity\G' | sudo mysql -uhbnb_dev -phbnb_dev_pwd hbnb_dev_db
+
+echo ""
