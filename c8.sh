043e8e05c1a496599a083486e8546dde7b05a17c
nuuX <cv.bouhlaoui@gmail.com>
2023-09-18 21:43:13 +0100

    Update c8.sh

--- c8.sh
+++ c8.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-rm place_id.txt
+
+# Get the directory where this script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Set the path to c7.sh in the same directory as this script
+c7_script="$script_dir/c7.sh"
+
+# Check if c7.sh exists
+if [ ! -f "$c7_script" ]; then
+    echo "Error: 'c7.sh' not found in the same directory as '$0'."
+    exit 1
+fi
+
+# Set the "exit on error" option
+set -e
+
 # Run Task 7 script before executing Task 8
-./c7.sh
+"$c7_script"
+
+# Reset the "exit on error" option (optional, depending on your script)
+set +e
+
+rm place_id.txt
+
+# Run Task 7 script before executing Task 8
+
 echo ""
 echo "--------------"
 echo "--- TASK8 ---"
@@ -9,24 +32,24 @@ echo "--------------"
 echo ""
 # Retrieve City ID from the file
 city_id=$(cat city_id.txt)
-echo "#--> City ID: $city_id"
+echo "City ID: $city_id"
 echo ""
 
 # Retrieve User ID from the file
 user_id=$(cat user_id.txt)
-echo "#--> User ID: $user_id"
+echo "User ID: $user_id"
 echo ""
 
 # Create a new Place using the captured City ID and User ID
 echo "#--> Creating a new Place with City ID: $city_id and User ID: $user_id..."
-place_output=$(echo 'create Place city_id="'$city_id'" user_id="'$user_id'" name="Lovely_place" number_rooms=3 number_bathrooms=1 max_guest=6 price_by_night=120 latitude=37.773972 longitude=-122.431297' | sudo -E HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db .//console.py)
+place_output=$(echo 'create Place city_id="'$city_id'" user_id="'$user_id'" name="Lovely_place" number_rooms=3 number_bathrooms=1 max_guest=6 price_by_night=120 latitude=37.773972 longitude=-122.431297' | sudo -E HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ~/AirBnB_clone_v2/console.py)
 place_id=$(echo "$place_output" | grep -o -E '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')
-echo "#--> Place ID: $place_id"
+echo "Place ID: $place_id"
 echo ""
 
 # List all Places
 echo "#--> Listing all Places..."
-echo 'all Place' | sudo -E HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db .//console.py
+echo 'all Place' | sudo -E HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ~/AirBnB_clone_v2/console.py
 
 echo ""
 # Execute SQL query to list all Places
